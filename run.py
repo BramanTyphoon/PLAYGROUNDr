adce057a81b585f064c4853a67b9d184c68706e2
James Bramante <James.F.Bramante@gmail.com>
2020-01-24 16:00:04 -0500

    Adding initial scripts and data.

---
+++ run.py
@@ -0,0 +1,98 @@
+#!/home/bramante/anaconda3/envs/insight/bin/ python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 17:22:45 2020
+
+@author: bramante
+"""
+
+from flask import render_template, request, Flask
+#from flask_bootstrap import Bootstrap
+from GooglePlaces import GooglePlaces
+from util import text_prepare, build_fasttext_model
+import numpy as np
+import pickle
+import json
+
+# Variables used within the other methods
+API_KEY = 'AIzaSyAUzvGNHIl8i3gY_GC54lJd-NJLq6LbNNA' # Key required for Google API use
+gp = GooglePlaces(API_KEY) # Object that interfaces with Google API to pull review data
+
+# Variables useful for map display
+init_origin = {"lat": 43.65, "lng": -79.38}
+init_zoom = 12
+
+
+# Define model parameters and load in the model files
+num_amenities = 7 # Number of amenities predicted by the classifier
+min_num_reviews = 4 # Minimimum number of reviews to accept before running model
+amenity_names = ['Playground','Splash pad','Pool','Ice rink','Restroom','Sports field', 'Dog park']
+model_file_name = "data/NBCmodel.mod" # Filename containing the classification model
+review_database_name = "data/park_reviews_database_20200121.json" # Filename of database containing all park reviews
+
+with open(model_file_name,'rb') as fp:
+    clf = pickle.load(fp)
+    
+w2v_model = build_fasttext_model(review_database_name)
+
+# Start the app instance
+app = Flask(__name__, template_folder="templates")
+#Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('mainmap.html', origin=json.dumps(init_origin), zoom=init_zoom,apikey = API_KEY,name = "Location Name", status = "Click on a location", address = "Address", amenities = zip(amenity_names,[False]*num_amenities))
+
+# This route gets called when a user has clicked on a location with a placeid
+@app.route('/park_amenities')
+def park_amenity():
+    # Extract Google Places details for the placeid
+    placeid = request.args.get('placeid')
+    inlon = float(request.args.get('inlon'))
+    inlat = float(request.args.get('inlat'))
+    zoom = float(request.args.get('zoom'))
+    
+    reviews = gp.place_reviews(placeid)
+    reviews = reviews['result']
+    
+    # If the place is not a park, don't run the model
+    if "park" not in reviews['types']:
+        out_name = reviews['name']
+        out_text = "This site is not a park"
+        out_amenities = [0]*num_amenities
+        out_address = reviews['formatted_address']
+    
+    # If there are no reviews, don't run the model    
+    elif 'reviews' not in reviews.keys():
+        out_name = reviews['name']
+        out_text = "No reviews available for this site"
+        out_amenities = [0]*num_amenities
+        out_address = reviews['formatted_address']
+    else:
+        # If there are too few reviews, don't run the model
+        reviews_text = [text_prepare(rev) for rev in [revi['text'] for revi in reviews['reviews']] if text_prepare(rev)]
+        if len(reviews_text) < min_num_reviews:
+            out_name = reviews['name']
+            out_text = "Insufficient (<4) reviews for this site."
+            out_amenities = [0]*num_amenities
+            out_address = reviews['formatted_address']
+        else:
+            out_name = reviews['name']
+            out_text = ""
+            out_address = reviews['formatted_address']
+            # Run the model on the reviews text and return the results
+            # Clean the text
+            reviews_text = ' '.join(reviews_text)
+            # Vectorize the text
+            X_vect = np.zeros([1,w2v_model.trainables.layer1_size])
+            for word in reviews_text.split():
+                X_vect += w2v_model[word]
+            X_vect = X_vect / len(reviews_text.split())
+            # Run the classification model
+            y_pred = clf.predict(X_vect[0,:].reshape(1,-1))
+            out_amenities = [y_pred[0,ii] for ii in range(y_pred.shape[1])]
+    
+    return render_template('mainmap.html',origin=json.dumps({"lat": inlat,"lng": inlon}), zoom=float(zoom),apikey=API_KEY, name = out_name, status = out_text, address = out_address, amenities = zip(amenity_names,[x == 1 for x in out_amenities]))
+        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
---
+++ util.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 14:43:52 2020
+
+@author: bramante
+"""
+
+#......................Utilities for the PLAYGROUNDr app.......................
+# Author: James Bramante
+# Date: January 24, 2020
+
+#import nltk
+#nltk.download('stopwords')
+from gensim.models import FastText
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+import re
+
+# Text preparation variables
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;\.\n]') # Symbols to replace in string before model application
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z ]') # More symbols to replace, this time with ""
+STOPWORDS = set(stopwords.words('english')) # Stop words to remove
+lemmatizer = WordNetLemmatizer()
+
+# Model development variables
+vector_size = 128
+window_size = 5
+train_epochs = 5
+
+# Method for preparing text for modeling
+def text_prepare(text):
+    text = text.lower()
+    text = re.sub(REPLACE_BY_SPACE_RE,' ',text)# replace REPLACE_BY_SPACE_RE symbols by space in text
+    text = re.sub(BAD_SYMBOLS_RE, '',text)# delete symbols which are in BAD_SYMBOLS_RE from text
+#    text = ' '.join([x for x in text.split() if x not in STOPWORDS])# delete stopwords from text and don't lemmatize
+    text = ' '.join([lemmatizer.lemmatize(x) for x in text.split() if x not in STOPWORDS])# delete stopwords from text and lemmatize
+    return text
+
+def build_fasttext_model(full_database_file):
+    review_database = pd.read_json(full_database_file,orient='records',lines='True')
+    X_vector_train = []
+    for review in list(review_database['reviews']):
+        for rev in review.split(r'|||'):
+            clean = re.split('[.!?;]',rev)
+            X_vector_train += [text_prepare(sent.replace('/n',' ')).split() for sent in clean if sent]
+            
+    # Train the word2vec model
+    # Let's create a basic word2vec model using our full review corpus
+    # Parameters
+    w2v_model = FastText(min_count=5,
+                         window=window_size,
+                         size=vector_size,
+                         workers=4)
+    w2v_model.build_vocab(X_vector_train)
+    w2v_model.train(X_vector_train,total_examples=w2v_model.corpus_count,epochs=train_epochs)
+    w2v_model.init_sims(replace=True)
+    return w2v_model.wv
