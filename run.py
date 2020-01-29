b6a651c14d3dd96934da41f2cc0d27dea443650d
James Bramante <James.F.Bramante@gmail.com>
2020-01-28 22:50:52 -0500

    Changed flask-html interface to use dynamically constructed html elements in a javascript function and AJAX communication.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ Created on Wed Jan 22 17:22:45 2020
 @author: bramante
 """
 
-from flask import render_template, request, Flask
+from flask import render_template, request, Flask, jsonify
 from GooglePlaces import GooglePlaces
 from util import text_prepare, bag_of_words_vectorize
 from flask_bootstrap import Bootstrap
@@ -15,7 +15,8 @@ import pickle
 import json
 
 # Variables used within the other methods
-API_KEY = 'AIzaSyAUzvGNHIl8i3gY_GC54lJd-NJLq6LbNNA' # Key required for Google API use
+#API_KEY = 'AIzaSyAUzvGNHIl8i3gY_GC54lJd-NJLq6LbNNA' # Key required for Google API use
+API_KEY = input("Provide a Google Places/Maps API KEY:")
 gp = GooglePlaces(API_KEY) # Object that interfaces with Google API to pull review data
 
 # Variables useful for map display
@@ -93,6 +94,59 @@ def park_amenity():
             out_amenities = [y_pred[0,ii] for ii in range(y_pred.shape[1])]
     
     return render_template('mainmap.html',origin=json.dumps({"lat": inlat,"lng": inlon}), zoom=float(zoom),apikey=API_KEY, name = out_name, status = out_text, address = out_address, amenities = zip(amenity_names,[x == 1 for x in out_amenities]))
+
+# This route gets called when a user has clicked on a location with a placeid
+@app.route('/singlepark', methods=['POST'])
+def single_park_amenities():
+    placeid = request.form['placeid']
+    
+    # Extract details with Google API
+    reviews = gp.place_reviews(placeid)
+    reviews = reviews['result']
+    
+    # If the place is not a park, don't run the model
+    if "park" not in reviews['types']:
+        out_name = reviews['name']
+        out_text = "This site is not a park"
+        out_scores = [0]*num_amenities
+        out_address = reviews['formatted_address']
+    
+    # If there are no reviews, don't run the model    
+    elif 'reviews' not in reviews.keys():
+        out_name = reviews['name']
+        out_text = "No reviews available for this site"
+        out_scores = [0]*num_amenities
+        out_address = reviews['formatted_address']
+    else:
+        # If there are too few reviews, don't run the model
+        reviews_text = [text_prepare(rev) for rev in [revi['text'] for revi in reviews['reviews']] if text_prepare(rev)]
+        if len(reviews_text) < min_num_reviews:
+            out_name = reviews['name']
+            out_text = "Insufficient (<4) reviews for this site."
+            out_scores = [0]*num_amenities
+            out_address = reviews['formatted_address']
+        else:
+            out_name = reviews['name']
+            out_text = ""
+            out_address = reviews['formatted_address']
+            # Run the model on the reviews text and return the results
+            # Clean the text
+            reviews_text = ' '.join(reviews_text)
+            # Vectorize the text
+            X_vect = bag_of_words_vectorize(reviews_text,bow_model)
+            X_vect = X_vect / len(reviews_text.split())
+            # Run the classification model
+            y_pred = clf.predict(X_vect[0,:].reshape(1,-1))
+            out_scores = [str(y_pred[0,ii]) for ii in range(y_pred.shape[1])]
+            
+    # Return JSON output
+    return jsonify({"results" : [{
+            'name' : out_name,
+            'text' : out_text,
+            'address' : out_address,
+            'scores' : out_scores,
+            'amenities' : amenity_names
+            }]})
         
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
