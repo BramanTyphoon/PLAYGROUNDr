5f73e3d847bcf06235bb42c0c47346e8806466ca
James Bramante <James.F.Bramante@gmail.com>
2020-01-30 11:33:58 -0500

    Changed app to application in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ Created on Thu Jan 30 11:28:24 2020
 """
 
 #............................Binding for Gunicorn..............................
-from run import app
+from run import application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
